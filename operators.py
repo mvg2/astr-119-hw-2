4049d4092ac34c47c764e3368ca0672c720203e2
Michael Gonzales <72222336+mvg2@users.noreply.github.com>
2020-10-14 15:26:59 -0700

    upload "operators.py" program

---
+++ operators.py
@@ -0,0 +1,36 @@
+x = 9           # assign integer values to vars x and y
+y = 3
+
+# Arithmetic Operators
+print(x+y)      # Addition
+print(x-y)      # Subtraction
+print(x*y)      # multiplication
+print(x/y)      # division
+print(x % y)    # modulus
+print(x**y)     # exponentiation
+x = 9.191823
+print(x//y)     # floor division
+
+# Assignment operators
+x = 9           # set x = 9
+x += 3          # set x = x + 3
+print(x)        # prints the current value of x
+x = 9
+x -= 3          # x = x - 3
+print(x)        # prints current value of x
+x *= 3          # x = x * 3
+print(x)
+x /= 3          # x = x / 3
+print(x)
+x **= 3         # x = x**3 (x to the third power)
+print(x)
+
+# Comparison operators
+x = 9
+y = 3
+print(x == y)     # True if x equals y, False if otherwise
+print(x != y)     # True if x does not equal y, false otherwise
+print(x > y)      # True if x is greater than y, false otherwise
+print(x < y)      # True if x is less than y, false otherwise
+print(x >= y)     # True if x is greater than or equal to y, false otherwise
+print(x <= y)     # True if x is less than or equal to y, false otherwise
