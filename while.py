00a4a2b4d47a00b5f4f0e58fe7056e2ce5ae4422
Michael Gonzales <72222336+mvg2@users.noreply.github.com>
2020-10-13 17:23:56 -0700

    upload hw file "while.py"

---
+++ while.py
@@ -0,0 +1,10 @@
+"""
+This program will continue adding 10 to i starting from i = 0
+and stopping once the value of i exceeds 119. It will print
+each result on a newline.
+"""
+i = 0           # define variable i as an integer
+
+while(i<119):   # while i is less than 119, do the following
+    print(i)    # print the current value of i
+    i += 10     # add 10 to i
